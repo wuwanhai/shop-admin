d1b483fa4a99086a00f6871e508ec678c209e743
wuwanhai <haiwanwu@outlook.com>
2022-05-17 21:25:08 +0800

    增加路径别名，以及 ts 路径配置

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,10 +4,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useStore } from "../../store";
-
+import User from '@/api/user'
 const msg = ref('我是首页')
 
 // 使用自定义的  use store
 const store = useStore()
 console.log(store.state.count)
+console.log(User)
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// @ts-ignore
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),vueJsx({
-    // 配置 TSX 支持
-    // options are passed on to @vue/babel-plugin-jsx
-  })]
+  plugins: [
+      vue(),
+      vueJsx({
+  })],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname,'src')
+    }
+  }
 })
