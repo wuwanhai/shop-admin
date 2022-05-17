b3bc960a597cfe296962421f8f53fde1d9107216
wuwanhai <haiwanwu@outlook.com>
2022-05-17 20:49:45 +0800

    增加 vuex 和 vuex 的类型推断

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router";
+import {store, key} from "./store";
 
-createApp(App).use(router).mount('#app')
+// 传入 store 同时传入 key “至关重要”
+createApp(App).use(router).use(store, key).mount('#app')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,8 +2,12 @@
    <h1>{{msg}}</h1>
 </template>
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue";
+import { useStore } from "../../store";
 
 const msg = ref('我是首页')
 
+// 使用自定义的  use store
+const store = useStore()
+console.log(store.state.count)
 </script>
