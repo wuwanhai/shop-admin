a48c53066f2732c48cb929f38ffda36f858835a2
wuwanhai <haiwanwu@outlook.com>
2022-05-19 17:49:19 +0800

    使用 type of 推导 ts 类型

--- src/layout/AppHeader/ToggleSidebar.vue
+++ src/layout/AppHeader/ToggleSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <el-icon :size="20" >
-    <i @click="$store.commit('setIsCollapse',!$store.state.isCollapse)">
+    <i @click="handleCollapse">
       <Expand v-if="$store.state.isCollapse"/>
       <Fold v-else/>
     </i>
@@ -9,6 +9,14 @@
 
 <script lang="ts" setup>
 import { Expand, Fold } from '@element-plus/icons-vue'
+import { useStore } from '@/store'
+// import {computed} from "vue";
+
+const store = useStore()
+const handleCollapse = () => {
+   store.commit('setIsCollapse',!store.state.isCollapse)
+}
+
 </script>
 
 <style scoped>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,27 @@
 import { createStore, Store,useStore as baseUseStore} from "vuex";
 import {InjectionKey} from "vue";
 
-//  state 的接口类型
 
-export interface State {
-    count: number,
-    isCollapse: boolean
+// state 数据
+const state = {
+    count: 0,
+    isCollapse: true
 }
 
+//  state 的接口类型
+// export interface State {
+//     count: number,
+//     isCollapse: boolean
+// }
+
+// 使用 type of 推导出来
+export type State = typeof state
 // 定义 injection key
 
 export const key: InjectionKey<Store<State>> = Symbol('store')
 
 export const store = createStore<State>({
-    state () {
-        return {
-            count: 0,
-            isCollapse: true
-        }
-    },
+    state,
     mutations: {
         increment (state) {
             state.count++
