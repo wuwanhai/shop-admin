bf4284912fdf2e9cf2182dc90f5f1f534685b09b
wuwanhai <haiwanwu@outlook.com>
2022-05-21 22:57:26 +0800

    增加退出

--- src/layout/AppHeader/UserInfo.vue
+++ src/layout/AppHeader/UserInfo.vue
@@ -9,7 +9,7 @@
     <template #dropdown>
       <el-dropdown-menu>
         <el-dropdown-item>个人中心</el-dropdown-item>
-        <el-dropdown-item>退出登录</el-dropdown-item>
+        <el-dropdown-item @click="logoutHandle">退出登录</el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
@@ -17,6 +17,40 @@
 
 <script lang="ts" setup>
 import { ArrowDown } from '@element-plus/icons-vue'
+import {ElMessage, ElMessageBox} from "element-plus";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+const logoutHandle = () => {
+  ElMessageBox.confirm(
+      '是否退出？',
+      'Warning',
+      {
+        confirmButtonText: 'OK',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }
+  )
+      .then(async () => {
+
+        // 退出
+        await router.push({
+          name: 'login'
+        })
+
+        await ElMessage({
+          type: 'success',
+          message: '退出成功',
+        })
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: 'Delete canceled',
+        })
+      })
+
+   }
 </script>
 <style scoped>
 .example-showcase .el-dropdown-link {
